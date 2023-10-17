9341c6772193dd2a1bf6500a3b1c4453dff6d0e4
Valentin Despa <valentin@vdespa.com>
2023-10-17 17:19:42 +0200

    Remove localhost

--- e2e/app.spec.js
+++ e2e/app.spec.js
@@ -2,14 +2,14 @@
 const { test, expect } = require('@playwright/test');
 
 test('has title', async ({ page }) => {
-  await page.goto('http://localhost:3000');
+  await page.goto('/');
 
   // Expect a title "to contain" a substring.
   await expect(page).toHaveTitle(/Learn Jenkins/);
 });
 
 test('has Jenkins in the body', async ({ page }) => {
-  await page.goto('http://localhost:3000');
+  await page.goto('/');
 
   await page.locator('a')
             .filter({ hasText: 'Learn Jenkins on Udemy' })
